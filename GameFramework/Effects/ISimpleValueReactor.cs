55ba794ff82a45d76b7d685f1d6e41b57907fe22
eliott <eliott.jaquier@gmail.com>
2025-12-20 13:36:31 +0100

    Add ISimpleValueReactor

---
+++ GameFramework/Effects/ISimpleValueReactor.cs
@@ -0,0 +1,18 @@
+﻿namespace GameFramework.Effects
+{
+    /// <summary>
+    /// Usefull for objects that react to simple value changes (like health, stamina, etc).
+    /// </summary>
+    public interface ISimpleValueReactor<T>
+    {
+        /// <summary>
+        /// Because you can have multiple ISimpleValueReactor<float>, this is a channel identifier to distinguish the value that they should react to.
+        /// </summary>
+        public string ReactorChannel { get; }
+        /// <summary>
+        /// Called when the value changes.
+        /// </summary>
+        /// <param name="newValue">The new value.</param>
+        public void OnValueChanged(T newValue);
+    }
+}
