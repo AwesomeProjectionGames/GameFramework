26f3ea4b92ae4c7a69431b27f30f6a4ec4180e2d
eliott <eliott.jaquier@gmail.com>
2025-12-23 18:51:44 +0100

    Improve IHaveUUID with IDRegistry

---
+++ GameFramework/Identification/IHaveUUID.cs
@@ -0,0 +1,23 @@
+﻿namespace GameFramework.Identification
+{
+    /// <summary>
+    /// Identifies an object with a unique UUID (Universally Unique Identifier).
+    /// Each instance of the same object should have a unique, different UUID.
+    /// In context of actors, this should be used to identify each instance of an actor in the game world (with the GameState for example).
+    /// It should be SYNCED OVER THE NETWORK to identify objects between server and clients.
+    /// It also SHOULD BE PERSISTENT between game sessions, to identify the same object when LOADING a saved game.
+    /// Every object implementing this interface should be retreivable by its UUID through a central registry (like a GameState or any IIDDatabase).
+    /// </summary>
+    public interface IHaveUUID
+    {
+        public string UUID { get; }
+        
+        /// <summary>
+        /// If's it's necessary to change the UUID (for example when duplicating an object)
+        /// this method can be used to attempt to set a new UUID.
+        /// </summary>
+        /// <param name="newUUID"></param>
+        /// <returns>>Returns true if the UUID was successfully changed, false otherwise.</returns>
+        public bool TryToChangeUUID(string newUUID);
+    }
+}
---
+++ GameFramework/Identification/IIDRegistry.cs
@@ -0,0 +1,16 @@
+﻿namespace GameFramework.Identification
+{
+    /// <summary>
+    /// Interface for a database that can retrieve objects by their unique identifier (UUID).
+    /// </summary>
+    public interface IIDRegistry
+    {
+        /// <summary>
+        /// Get an object by its unique identifier (UUID).
+        /// </summary>
+        /// <param name="id">The unique identifier of the object.</param>
+        /// <typeparam name="T">The type of the object to retrieve, must implement IHaveUUID.</typeparam>
+        /// <returns>The object with the specified UUID (of type T) if found; otherwise, null.</returns>
+        T GetByID<T>(string id) where T : IHaveUUID;
+    }
+}
