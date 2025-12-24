e2f743bfc24905adaae80a3b1cfaff58e88fc52b
eliott <eliott.jaquier@gmail.com>
2025-12-24 01:40:28 +0100

    Add IActorComponent interface for reusable actor behavior

---
+++ GameFramework/IActorComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace GameFramework.GameFramework
+{
+    /// <summary>
+    /// Components that add reusable behavior to an IActor should implement this interface.
+    /// If implemented by a MonoBehaviour, this interface should be considered more restrictive as it imposes that this should be directly on the actor / entity (or a child object).
+    /// </summary>
+    public interface IActorComponent
+    {
+        IActor GetActor();
+    }
+}
