0fd43cf2752b08698c46959a535820551a45c7d0
eliott <eliott.jaquier@gmail.com>
2026-01-23 19:11:11 +0100

    Use IActorComponent with Actor property instead of custom PawnOwningThis

--- GameFramework/Interaction/IInteractor.cs
+++ GameFramework/Interaction/IInteractor.cs
@@ -1,11 +1,11 @@
-﻿
+﻿using GameFramework.Dependencies;
+
 namespace GameFramework.Interaction {
     /// <summary>
     /// Interface for interactable objects in the interaction system.
     /// </summary>
-    public interface IInteractor
+    public interface IInteractor : IActorComponent
     {
-        public IPawn PawnOwningThis { get; }
         public void Interact();
     }
 }
