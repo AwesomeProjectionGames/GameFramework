355f1a1054f1fe9774c8f3d6b3146088cbe6f297
eliott <eliott.jaquier@gmail.com>
2026-02-10 01:16:56 +0100

    ICameraController now support setting the viewport size

--- GameFramework/Spectating/ICameraController.cs
+++ GameFramework/Spectating/ICameraController.cs
@@ -26,14 +26,24 @@ namespace GameFramework
     {
         /// <summary>
         /// The currently active camera (or null if no camera is active).
+        /// It should be a virtual camera that can be copied to the master camera of this controller.
+        /// It should copy everything except the viewport rect, which is managed by the controller itself.
         /// </summary>
         ICamera? CurrentCamera { get; }
+        
         void TransitionToCamera(ICamera cameraTarget);
+        
         /// <summary>
         /// Transition to the specified camera with the given transition settings.
         /// </summary>
         /// <param name="cameraTarget">The target camera to transition to.</param>
         /// <param name="transitionSettings">Settings for the camera transition.</param>
         void TransitionToCamera(ICamera cameraTarget, CameraTransitionSettings transitionSettings);
+        
+        /// <summary>
+        /// Set the viewport rect for the camera controlled by this controller.
+        /// </summary>
+        /// <param name="viewPort">The viewport rect to set for the camera. Values should be between 0 and 1, where (0,0) is the bottom-left corner of the screen and (1,1) is the top-right corner.</param>
+        void SetViewPort(Rect viewPort);
     }
 }
