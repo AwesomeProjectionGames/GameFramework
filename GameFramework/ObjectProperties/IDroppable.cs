e6682ca912469cd45a2212137b41f80b88e69d4f
eliott <eliott.jaquier@gmail.com>
2026-04-12 00:13:34 +0200

    Add Drop method overloads to IDroppable interface and implement in PhysicsPicker and PickableItemGameObject

--- GameFramework/ObjectProperties/IDroppable.cs
+++ GameFramework/ObjectProperties/IDroppable.cs
@@ -4,6 +4,7 @@ namespace GameFramework
 {
     public interface IDroppable
     {
+        public void Drop(IEntity instigator);
         public void Drop(IEntity instigator, Vector3 dropWorldPosition, Quaternion dropWorldRotation, Vector3 dropVelocity = default);
     }
 }
