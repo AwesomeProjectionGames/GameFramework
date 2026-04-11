5adb98d4fda3a80a2f7c32484299014571e34ac3
eliott <eliott.jaquier@gmail.com>
2026-04-11 03:34:17 +0200

    Add interfaces for component accessors in Game Framework

--- GameFramework/IEntity.cs
+++ GameFramework/IEntity.cs
@@ -8,10 +8,8 @@ namespace GameFramework
     /// The base class for an Object that has significant presence in the game world / has some functionality
     /// Entities can have specials features to compose its behavior through components (see IComponentsContainer) and can dispatch events through its own event bus (see IEventBus).
     /// </summary>
-    public interface IEntity
+    public interface IEntity : ITransform
     {
-        Transform Transform { get; }
-
         /// <summary>
         /// The event bus associated with this entity. Like a Event Dispatcher owned by an Actor in unreal terminology.
         /// It should be used to dispatch events related to this entity only and should follow the same lifetime as the entity itself.
---
+++ GameFramework/ObjectProperties/IComponentAccessors.cs
@@ -0,0 +1,42 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFramework
+{
+    public interface IPhysics
+    {
+        Rigidbody Rigidbody { get; }
+    }
+    
+    public interface ICollider
+    {
+        Collider Collider { get; }
+    }
+    
+    public interface IColliders
+    {
+        IEnumerable<Collider> Colliders { get; }
+    }
+    
+    public interface IRenderer
+    {
+        Renderer Renderer { get; }
+    }
+    
+    public interface IRenderers
+    {
+        IEnumerable<Renderer> Renderers { get; }
+    }
+    
+    public interface ITransform
+    {
+        Transform Transform { get; }
+    }
+
+    public interface IInstigator
+    {
+        IEntity? Instigator { get; }
+    }
+}
